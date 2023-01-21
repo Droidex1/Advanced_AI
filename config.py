6bf84bd954a0c808be3c888de550f4a2df322682
Prabhu K <105587688+Droidex1@users.noreply.github.com>
2023-01-21 21:51:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-AI_API_KEY = getenv("AI_API_KEY", "")
-AI_ID = int(getenv("AI_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "5869455480:AAEytYe4pTUTPVReyanJZw2ryJDpWzVjWvo")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Onemanhost:8122949387@cluster0.m1wmxne.mongodb.net/?retryWrites=true&w=majority")
+AI_API_KEY = getenv("AI_API_KEY", "pIqDytq3U6Q2GGyh")
+AI_ID = int(getenv("AI_ID", "172095"))

02a1f4877101ff94854038c6714217e3adb11f90
Prabhu K <105587688+Droidex1@users.noreply.github.com>
2023-02-12 19:20:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from os import getenv
 
 BOT_TOKEN = getenv("BOT_TOKEN", "5869455480:AAEytYe4pTUTPVReyanJZw2ryJDpWzVjWvo")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Onemanhost:8122949387@cluster0.m1wmxne.mongodb.net/?retryWrites=true&w=majority")
-AI_API_KEY = getenv("AI_API_KEY", "pIqDytq3U6Q2GGyh")
+AI_API_KEY = getenv("AI_API_KEY", "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")
 AI_ID = int(getenv("AI_ID", "172095"))
